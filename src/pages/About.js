4bc5eb12ea7ea810f9a4d5c0be51d35f5f8c53f7
Marco Upia <markremembers@gmail.com>
2023-12-03 19:55:43 -0500

    code-a-long

---
+++ src/pages/About.js
@@ -0,0 +1,16 @@
+import NavBar from "../components/NavBar";
+
+function About() {
+    return (
+        <>
+            <header>
+                <NavBar />
+            </header>
+            <main>
+                <h1>This is my about components!</h1>
+            </main>
+        </>
+    );
+};
+
+export default About;
